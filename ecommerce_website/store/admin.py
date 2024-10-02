4bb86fce5a677437d8636343174e9ca6d80d43e8
Sundar Khanal <sundarkhanal62@gmail.com>
2024-10-02 20:10:50 +0545

    Let's go

--- ecommerce_website/store/admin.py
+++ ecommerce_website/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *  # * means import everything from models.py
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
--- ecommerce_website/store/models.py
+++ ecommerce_website/store/models.py
@@ -2,3 +2,48 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank= True, on_delete= models.CASCADE)    #Allows the field to be left empty in forms.
+    name = models.CharField(max_length=200, null= True)     #null = true means It tells the database that it’s okay to store NULL in the column when no value is provided.
+    email = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    Customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank= True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=True)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):  #The __str__() function controls what should be returned when the class object is represented as a string.
+        return str(self.id)
+    
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL,null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null= True )
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class ShippingAddress(models.Model):
+    Customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
